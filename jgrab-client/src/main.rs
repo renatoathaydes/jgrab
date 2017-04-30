86f3b0f41329b86620cbdf475953af924d7ead5b
renato <renato@twobo.com>
2017-04-30 23:22:01 +0200

    Improved Rust client connection retry.

--- jgrab-client/src/main.rs
+++ jgrab-client/src/main.rs
@@ -4,18 +4,24 @@ use std::io::Read;
 use std::io::Write;
 use std::env;
 use std::str;
+use std::option::Option;
 use std::process::Command;
+use std::process::Child;
 use std::path::PathBuf;
 use std::thread::sleep;
 use std::time::Duration;
+use wait_timeout::ChildExt;
+use std::io::Error;
+
+extern crate wait_timeout;
+
+const MAX_RETRIES: usize = 5;
 
 fn main() {
-    let max_retries: usize = 3;
-
     let args: Vec<String> = env::args().collect();
     let message = create_message(&args);
 
-    send_message(message.as_bytes(), max_retries);
+    send_message_retrying(message.as_bytes());
 }
 
 fn create_message(args: &Vec<String>) -> String {
@@ -32,34 +38,65 @@ fn create_message(args: &Vec<String>) -> String {
     message
 }
 
-fn send_message(socket_message: &[u8], max_retries: usize) {
-    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:5002") {
-        stream.write(socket_message).unwrap();
+fn send_message_retrying(message: &[u8]) {
+    if let Some(_) = send_message(message, false) {
+        // failed to connect, try to start the daemon, then retry
+        let mut retries = MAX_RETRIES;
 
-        let mut client_buffer = [0u8; 1024];
+        let mut child = start_daemon();
+        check_status(&mut child);
 
-        loop {
-            match stream.read(&mut client_buffer) {
-                Ok(n) => {
-                    if n == 0 {
-                        break;
-                    } else {
-                        stdout().write(&client_buffer[0..n]).unwrap();
-                    }
-                }
-                Err(err) => error(&err.to_string())
+        while retries > 0 {
+            if let Some(err) = send_message(message, true) {
+                check_status(&mut child);
+
+                log(&format!("unable to connect to JGrab daemon: {}", err));
+                log(&format!("will re-try {} more times", &mut retries));
+                retries -= 1;
+            } else {
+                break // success
             }
         }
-    } else if max_retries > 0 {
-        start_daemon();
-        send_message(socket_message, max_retries - 1);
-    } else {
-        error("unable to start JGrab daemon. \
-               Make sure JGrab's port [5002] is not already bound")
+
+        if retries == 0 {
+            error("unable to start JGrab daemon. \
+                   Make sure JGrab's port [5002] is not already bound");
+        }
     }
 }
 
-fn start_daemon() {
+fn send_message(socket_message: &[u8],
+                is_retry: bool) -> Option<Error> {
+    match TcpStream::connect("127.0.0.1:5002") {
+        Ok(mut stream) => {
+            if is_retry {
+                log("Connected!");
+            }
+
+            stream.write(socket_message).unwrap();
+
+            let mut client_buffer = [0u8; 1024];
+
+            loop {
+                match stream.read(&mut client_buffer) {
+                    Ok(n) => {
+                        if n == 0 {
+                            break;
+                        } else {
+                            stdout().write(&client_buffer[0..n]).unwrap();
+                        }
+                    }
+                    Err(err) => error(&err.to_string())
+                }
+            }
+
+            Option::None
+        }
+        Err(err) => Option::Some(err)
+    }
+}
+
+fn start_daemon() -> Child {
     log("Starting daemon");
 
     if let Some(user_home) = env::home_dir() {
@@ -75,9 +112,9 @@ fn start_daemon() {
                 .spawn();
 
             match cmd {
-                Ok(process) => {
-                    log(&format!("Daemon has been started, pid={}", process.id()));
-                    sleep(Duration::from_millis(500))
+                Ok(child) => {
+                    log(&format!("Daemon started, pid={}", child.id()));
+                    child
                 }
                 Err(err) => error(&err.to_string())
             }
@@ -87,6 +124,21 @@ fn start_daemon() {
         }
     } else {
         error("user.home could not be found, making it impossible to start the JGrab Daemon");
+    }
+}
+
+fn check_status(child: &mut Child) {
+    let timeout = Duration::from_secs(1);
+
+    match child.wait_timeout(timeout) {
+        Ok(Some(status)) => error(&format!(
+            "The JGrab daemon has died prematurely, exit code was {}", status)),
+        Ok(None) => {
+            // give time for the socket server to become active
+            sleep(Duration::from_secs(1));
+        }
+        Err(e) => error(&format!(
+            "Could not wait for JGrab daemon process status: {}", e)),
     }
 }
 
